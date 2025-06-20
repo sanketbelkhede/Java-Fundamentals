a05be7bdc86fab47faca19e8f03aa4cd7611c960
Sanket Belkhede <sanketbelkhede01.com>
2025-06-20 20:03:11 +0530

    Revert "random"
    This reverts commit 4fb9ebed0a3e286c4eada6749f8e52114aae6be0.

--- src/schoolconstuctor/Demo.java
+++ src/schoolconstuctor/Demo.java
@@ -15,8 +15,6 @@ public class Demo {
 	public static void main(String[] args) {
 //		Demo obj = new Demo();
 //		obj.display();
-		System.out.println(Character.isLetter(','));
-//		System.out.println(Character.is);
 	}
 	
 	void display() {
